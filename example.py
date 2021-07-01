2726aeaa6f40c0ef7c1ccbe7655d422e99c7fc31
Nekit3138 <nikitaserov912@gmail.com>
2021-07-01 10:22:12 +0300

    change example class

--- example.py
+++ example.py
@@ -3,7 +3,7 @@ from preprocessing import Preprocessor
 from cv2 import imshow, waitKey
 
 path = 'bears_data/withBears/_2016-04-25 11-06-03_2568_L.JPG'
-Preprocessor.set_file(path)
+Preprocessor = Preprocessor(path)
 
 dots = Preprocessor.get_dots()
 print(*dots, sep='\n')
